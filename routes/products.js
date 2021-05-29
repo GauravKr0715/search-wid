88c630902f42f110010beafc39878c45f6075b3e
GauravKr0715 <46153627+GauravKr0715@users.noreply.github.com>
2021-05-29 10:50:47 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -19,6 +19,7 @@ Router.get("/", async (req, res) => {
         const retrievedProducts = await Store.find({
             "products.name": { $regex: `(?i)^${req.query.name}+.*$` },
         });
+        //2214: taking all to lower case
         let charact = req.query.name.toLowerCase();
         const re = RegExp(`^${charact}+.*`, "i");
         // let matchChar = /\${charact}+.*/;
@@ -54,6 +55,7 @@ Router.get("/", async (req, res) => {
             });
         });
         // console.log(result);
+        //709: old code... do not delete
         // const retrivedProducts = await Product.find({
         //     name: { $regex: `(?i)^${req.query.name}+.*$` },
         // });
